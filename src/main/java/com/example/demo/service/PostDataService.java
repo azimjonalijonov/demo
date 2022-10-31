3afd4271554964885a78cdadcc335ffc159715b4
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-10-31 14:57:34 +0500

    new

---
+++ src/main/java/com/example/demo/service/PostDataService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import com.example.demo.Entity.PostData;
+import com.example.demo.model.Post;
+import com.example.demo.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostDataService {
+private final PostRepository postRepository;
+
+    public PostDataService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+    public PostData save(PostData postData){
+        PostData res = postRepository.save(postData);
+        return res;
+    }
+    public List<PostData> saveAll(Post[] posts){
+        List<PostData> postDataList =new ArrayList<>();
+                for(Post post:posts){
+                    PostData postData =new PostData();
+                    postData.setPostId(post.getId());
+                    postData.setUserId(post.getUserId());
+                    postData.setTitle(post.getTitle());
+                    postData.setBody(post.getBody());
+                    postDataList.add(postData);
+                }
+
+        return postRepository.saveAll(postDataList);
+
+    }
+
+
+
+  }
+
+
