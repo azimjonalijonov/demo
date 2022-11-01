ad5cea073fc37f349f478c0cbd63b4afc54a1ff9
alijonoff <2110040@newuzbekistanuniversity.uz>
2022-11-01 20:37:53 +0500

    new

---
+++ src/main/java/com/example/demo/rest/PostResource.java
@@ -0,0 +1,34 @@
+package com.example.demo.rest;
+
+import com.example.demo.model.Post;
+import com.example.demo.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class PostResource {
+    private final PostService postService;
+
+    public PostResource(PostService postService) {
+        this.postService = postService;
+
+    }
+    @PostMapping("/posts")
+    public ResponseEntity save(@RequestBody Post post){
+        Post result =postService.save(post);
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/post")
+    public ResponseEntity show(@RequestParam Long postId){
+        List<Post> result =postService.findAllByQueryParam(postId);
+        return ResponseEntity.ok(result);
+    }
+    @GetMapping("/posts")
+    public ResponseEntity findAll(){
+        Object result = postService.findAll();
+        return ResponseEntity.ok(result);
+    }
+}
---
+++ src/main/java/com/example/demo/service/PostService.java
@@ -0,0 +1,72 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Post;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class PostService {
+    private final RestTemplate restTemplate;
+    private final PostDataService postDataService;
+
+    @Value("${api.placeholder}")
+    private String api;
+
+    public PostService(RestTemplate restTemplate, PostDataService postDataService) {
+        this.restTemplate = restTemplate;
+        this.postDataService = postDataService;
+
+    }
+    public Post save(Post post){
+        HttpHeaders headers =new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<Post> entity =new HttpEntity<>(post,headers);
+        Post result =restTemplate.postForObject(api+"/posts",entity,Post.class);
+        return result;
+    }
+    public  Post update(Long id, Post post){
+        HttpHeaders headers =new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity <Post> entity =new HttpEntity<>(post,headers);
+        Post result = restTemplate.exchange(api+"/posts"+id+"/comments",HttpMethod.PUT,entity,Post.class).getBody();
+        return  result;
+    }
+    public List<Post> findAllByQueryParam(Long postId){
+        HttpHeaders headers =new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity  <List<Post>> entity =new HttpEntity<>(headers);
+        String uriTemplate = UriComponentsBuilder.fromHttpUrl(api+"/comments")
+                .queryParam("postId","{postId}")
+                .encode()
+                .toUriString();
+        Map<String, Object> params =new HashMap<>();
+        params.put("postId",postId);
+
+        List<Post>  result = restTemplate.exchange(uriTemplate,HttpMethod.GET,entity,List.class,params).getBody();
+        return result;
+    }
+
+
+    public Object  findAll() {
+         HttpHeaders headers = new HttpHeaders();
+         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+         HttpEntity<Post[]> entity =new HttpEntity<>(headers);
+         Post[] result =restTemplate.exchange(this.api+"/posts", HttpMethod.GET,entity,Post[].class).getBody();
+         postDataService.saveAll(result);
+         return result;
+
+     }
+
+
+}
